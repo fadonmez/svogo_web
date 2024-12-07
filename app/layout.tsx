ab1459ca5c5604959c5e3c0304464818a3785069
Fatih Donmez <fatihdonmezbusiness@outlook.com>
2024-12-08 00:04:40 +0300

    chore: remove favicon.ico and update metadata to include new icon paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Svogo",
   description: "Learn your own way",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
 };
 
 const baloo = Baloo_2({
